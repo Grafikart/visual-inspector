bc02eef0a13d663e2bdf77f4efc932a160868b55
Jonathan <contact@grafikart.fr>
2026-03-21 22:26:04 +0100

    ci: github page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import tailwindcss from '@tailwindcss/vite'
+
+const base = process.env.BASE_PATH ?? '/'
+
 export default defineConfig({
+  base,
   plugins: [
     preact(),
     tailwindcss(),
